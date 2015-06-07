0e1997a79e9cc3b956769c8f48a4840f96651fc8
Juan Eduardo Mendoza <mendoza.juan.eduardo@gmail.com>
2015-06-07 13:31:20 -0300

    modifico

--- src/main/java/api/controller/ApiController.java
+++ src/main/java/api/controller/ApiController.java
@@ -21,7 +21,8 @@ public class ApiController {
         	   "<p><b>Numero actual de ventas: </b>" + ValueGetterResolver.getValue()+"</p>"+
         	 "<h3>Operaciones:</h3>"+
         	   "<p>/config?delta=n : Configura el tiempo de entrada de una nueva venta</p>" +
-        	 "<p>/value : retorna el valor actual</p>";
+        	 "<p>/value : retorna el valor actual</p>"+
+        	 "<p>/reset : reseta el numero de ventas a 0</p>";
     }
     
     @RequestMapping("/value")
@@ -33,6 +34,12 @@ public class ApiController {
     public String getValue(@RequestParam("delta") int delta) {
     	ConfigResolver.configureDelta(delta);
         return "Se configuro el delta a :" + delta;
+    }
+    
+    @RequestMapping(value = "/reset")
+    public String reset() {
+    	ValueGetterResolver.value = 0;
+        return "Se reseteo el numero de ventas a 0";
     }
     
 }
