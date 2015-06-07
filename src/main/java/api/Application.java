a49c6c950b00a4ae0d3c328facf48deca77560fb
Juan Eduardo Mendoza <mendoza.juan.eduardo@gmail.com>
2015-06-07 11:01:58 -0300

    modifico

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import api.controller.ApiController;
+
 @SpringBootApplication
 public class Application {
     
@@ -13,6 +15,13 @@ public class Application {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
+        
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        System.setProperty("server.port", webPort);
+        SpringApplication.run(ApiController.class, args);
         
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
